f0877e8ca09e3b645bda02b523d79270e9854b76
vd-vaishoo <vaishnavi.r@vuedata.in>
2023-11-17 15:29:52 +0530

    Your commit message here

---
+++ 1. HTML Layouts/script.js
@@ -0,0 +1,22 @@
+// var i = 1;
+// while (i <= 5) {
+//   var j = 1;
+//   while (j <= 5) {
+//     document.write('*');
+//     j++;
+//   }
+//   document.write('<br>');
+//   i++;
+// }
+
+var i = 1;
+var j = 1;
+while (i <= 5) {
+  
+  while (j <= 5) {
+    document.write('*');
+    j++;
+  }
+  document.write('<br>');
+  i++;
+}
